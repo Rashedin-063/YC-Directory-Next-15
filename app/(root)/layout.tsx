78d3ca981c04aa77db2fa4f1e0dbdc27c892b643
Rashedin-063 <rashedinislam.06@gmail.com>
2024-12-23 23:05:30 +0600

    metadata updated

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,10 @@
+import Navbar from "../components/Navbar";
+
+export default function Layout({ children }: Readonly<React.ReactNode>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar/>
+      {children}
+    </main>
+  )
+}
