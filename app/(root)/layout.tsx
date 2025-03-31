00fb5cab8e8fb7320ece9422577ae4679158c19a
Rashedin-063 <rashedinislam.06@gmail.com>
2025-03-31 21:56:59 +0600

    updated

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/Navbar";
 
-export default function Layout({ children }: Readonly<React.ReactNode>) {
+export default function Layout({ children }: <React.ReactNode>) {
   return (
     <main className="font-work-sans">
       <Navbar/>
