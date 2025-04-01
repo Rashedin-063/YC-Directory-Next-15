e82f6e6f50d423a0d00c0e9706b56b66adc93ed3
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-01 21:12:49 +0600

    footer style updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div>
+    <div className="container flex justify-center items-center p-4 bg-slate-900 text-gray-300 text-sm">
       2025 @all rights reserved
     </div>
   )
