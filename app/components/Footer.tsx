3a4800a405ff2fd0bbdbc8374a3fdba731435149
Rashedin-063 <rashedinislam.06@gmail.com>
2025-04-16 00:12:29 +0600

    footer updated

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,8 +1,23 @@
 const Footer = () => {
   return (
-    <div className="container flex justify-center items-center p-4 bg-slate-900 text-gray-300 text-sm">
-      2025 @all rights reserved
-    </div>
-  )
-}
-export default Footer
+    <footer className='bg-slate-900 text-gray-300 py-6'>
+      <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
+        <div className='text-sm'>&copy; 2025 All rights reserved</div>
+        <div className='flex space-x-4 mt-4 md:mt-0'>
+          <a href='#' className='hover:text-white'>
+            Privacy Policy
+          </a>
+          <a href='#' className='hover:text-white'>
+            Terms of Service
+          </a>
+          <a href='#' className='hover:text-white'>
+            Contact Us
+          </a>
+        </div>
+       
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
